86372cfe0223d782a281653ef4bbc0f7b20b3579
Aditi Nimbarte <aditinimbarte341@gmail.com>
2020-06-11 03:50:46 +0530

    Number and Marksheet file added in Python_Task1

---
+++ Python_Task1/number.py
@@ -0,0 +1,17 @@
+#store inputted integer value
+n1=int(input("enter first integer value:"))
+n2=int(input("enter second integer value:"))
+#store inputted float value
+f1=float(input("enter first float value:"))
+f2=float(input("enter second float value:"))
+#operations on integers
+print("\n sum of integer:",n1+n2)
+print("\nsubtraction of integer:",n1-n2)
+print("\nmultiplication of integer:",n1*n2)
+print("\ndivision of integer:",n1/n2)
+#operations on floats
+print("\nsum of float variables:",f1+f2)
+print("\n subtraction of float variables:",f1-f2)
+print("\nmultiplication of float variables:",f1*f2)
+print("\ndivision of float variables:",f1/f2)
+print("\nexponent of float variables:",f1**f2)
