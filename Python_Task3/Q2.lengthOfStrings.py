81e157e7425213ddf727a867591b7f970056d5ab
Aditi Nimbarte <aditinimbarte341@gmail.com>
2020-06-25 20:47:26 +0530

    file Q2.concatinating string is added

---
+++ Python_Task3/Q2.lengthOfStrings.py
@@ -0,0 +1,9 @@
+#taking two strings input
+string1=input("enter the first string: ")
+string2=input("enter the second string: ")
+
+#concatinating strings
+string = string1 + " " + string2
+
+#printing the length of string
+print("concatinated string length : ", len(string))
