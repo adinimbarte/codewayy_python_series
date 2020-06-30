bede3676bc4c8be967abf2d2dcf8ecfecc34503b
Aditi Nimbarte <aditinimbarte341@gmail.com>
2020-06-30 10:59:07 +0530

    task5 Q1 updated

--- Python-Task5/1.errorHandling/errorHandling.py
+++ Python-Task5/1.errorHandling/errorHandling.py
@@ -5,14 +5,14 @@ try:
     num1 = int(input("Enter any number:"))
     num2 = int(input("enter any number:"))
     
-    # checking if they are integer or not
-    if type(num1 or num2) == int:
-        z = num1 + num2
-        print(z)
+    z = num1 + num2
+    print(z)
     
     # created raise just to check working 
-    raise ValueError("Error:program is executed successfully")
+    raise ValueError("Error:try is executed succesfully")
 
 # if above code failed exception will be printed
 except ValueError as e:
     print(e)
+finally:
+    print("program is finally executed")
